d14e0e8cf9f2287a4c06754951edc2a97253990b
Yogendra Prashad <prashadyogendra5@gmail.com>
2026-09-25 16:21:09 +0530

    feat/realtime order updates on admin dashboard via socket.io

--- socket-server/index.js
+++ socket-server/index.js
@@ -6,6 +6,7 @@ import axios from "axios"
 dotenv.config()
 
 const app=express()
+app.use(express.json())
 
 const server=http.createServer(app)
 const port=process.env.PORT || 5000
@@ -37,6 +38,17 @@ io.on("connection",(socket)=>{
         console.log("user disconnected",socket.id)
 
     })
+})
+
+app.post("/notify",(req,res)=>{
+    const {event,data,socketId}=req.body
+    if(socketId){
+        io.to(socketId).emit(event,data)
+    }else{
+        io.emit(event,data)
+    }
+
+    return res.status(200).json({"success":true})
 })
 
 
